edbf715cf4203d8be35d5950c94a3a76f5452d03
Christine Chaewon Shin <chshin0225@gmail.com>
2023-08-15 23:39:40 +0900

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus deserunt sed officiis reiciendis commodi eaque
+      atque autem? Sit repellat neque molestias rerum minima tenetur porro accusamus, quo voluptatibus eligendi ratione.
+    </p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus deserunt sed officiis reiciendis commodi eaque
+      atque autem? Sit repellat neque molestias rerum minima tenetur porro accusamus, quo voluptatibus eligendi ratione.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
